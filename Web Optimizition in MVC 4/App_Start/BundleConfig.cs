9cd5383a00e76323a1ccfb53e2673cae1edcd5e6
aly ba <alybas2002@yahoo.fr>
2014-04-28 09:39:36 +0200

    rex

---
+++ Web Optimizition in MVC 4/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Optimization
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css")
+                        .Include("~/Content/bootstrap.css")
+                        .Include("~/Content/styles.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr")
+                        .Include("~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/js")
+                        .Include("~/Scripts/jquery*")
+                        .Include("~/Scripts/mustache.js")
+                        .Include("~/Scripts/bootstrap.js"));
+
+            var animalsBundle = new ScriptBundle("~/bundles/animals")
+                .Include("~/Scripts/animals.coffee");
+            animalsBundle.Transforms.Insert(0, new CoffeeBundler.CoffeeBundler());
+            bundles.Add(animalsBundle);
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
