d91e581d0dedb69b40bde3b3780ae050aae94e06
aly ba <alybas2002@yahoo.fr>
2014-12-18 19:54:17 +0100

    mef overview
    old mef before ninject

---
+++ examples of extenions to IEnumerable/MEFloader.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using CarRental.Data;
+using CarRental.Data.Data_Repositories;
+
+
+namespace CarRental.Business.Bootstrapper
+{
+    
+        public static class MEFLoader
+        {
+            public static CompositionContainer Init()
+            {
+                AggregateCatalog catalog = new AggregateCatalog();
+
+                catalog.Catalogs.Add(new AssemblyCatalog(typeof(AccountRepository).Assembly));
+                catalog.Catalogs.Add(new AssemblyCatalog(typeof(CarRepository).Assembly));
+                //catalog.Catalogs.Add(new AssemblyCatalog(typeof(CarRentalEngine).Assembly));
+
+                CompositionContainer container = new CompositionContainer(catalog);
+
+                return container;
+            }
+        }
+}
